27ccc002022333ba88bb5bde8de0b7416be03935
Derek Ogle <dogle@northland.edu>
2016-07-19 12:35:07 -0500

    Updated VBGF2 module

--- Exercises/Nunavut/VBGF2.R
+++ Exercises/Nunavut/VBGF2.R
@@ -6,48 +6,48 @@ library(nlstools) # for nlsBoot()
 library(readxl)
 
 source("PG008_readdata.R")
-dNU_FW <- filterD(dNU,!is.na(FL),!is.na(age),water.type %in% c("freshwater","Freshwater"),year==2007)
+dNU_FW07 <- filterD(dNU,!is.na(FL),!is.na(age),water.type %in% c("freshwater","Freshwater"),year==2007)
 # Removed some obvious outliers
-dNU_FW <- filterD(dNU_FW,!(age>10 & FL<200))
+dNU_FW07 <- filterD(dNU_FW07,!(age>10 & FL<200))
 
 xlbl <- "Age (yrs)"
 ylbl <- "Fork Length (in)"
 clr1 <- c("black","blue")
 clr2 <- col2rgbt(clr1,1/5)
 
-plot(FL~age,data=dNU_FW,pch=19,col=clr2[sex],xlab=xlbl,ylab=ylbl)
+plot(FL~age,data=dNU_FW07,pch=19,col=clr2[sex],xlab=xlbl,ylab=ylbl)
 
-( svOm <- vbStarts(FL~age,data=dNU_FW,plot=TRUE) )
+( svOm <- vbStarts(FL~age,data=dNU_FW07,plot=TRUE) )
 ( svLKt <- Map(rep,svOm,c(2,2,2)) )
 vbLKt <- FL~Linf[sex]*(1-exp(-K[sex]*(age-t0[sex])))
-fitLKt <- nls(vbLKt,data=dNU_FW,start=svLKt)
+fitLKt <- nls(vbLKt,data=dNU_FW07,start=svLKt)
 residPlot(fitLKt)
 
-bootLKt <- nlsBoot(fitLKt,niter=100)
+bootLKt <- nlsBoot(fitLKt,niter=1000)
 cbind(Ests=coef(fitLKt),confint(fitLKt))
 
 vbOm <- FL~Linf*(1-exp(-K*(age-t0)))
-fitOm <- nls(vbOm,data=dNU_FW,start=svOm)
+fitOm <- nls(vbOm,data=dNU_FW07,start=svOm)
 
 vbLK <- FL~Linf[sex]*(1-exp(-K[sex]*(age-t0)))
 svLK <- Map(rep,svOm,c(2,2,1))
-fitLK <- nls(vbLK,data=dNU_FW,start=svLK)
+fitLK <- nls(vbLK,data=dNU_FW07,start=svLK)
 vbLt <- FL~Linf[sex]*(1-exp(-K*(age-t0[sex])))
 svLt <- Map(rep,svOm,c(2,1,2))
-fitLt <- nls(vbLt,data=dNU_FW,start=svLt)
+fitLt <- nls(vbLt,data=dNU_FW07,start=svLt)
 vbKt <- FL~Linf*(1-exp(-K[sex]*(age-t0[sex])))
 svKt <- Map(rep,svOm,c(1,2,2))
-fitKt <- nls(vbKt,data=dNU_FW,start=svKt)
+fitKt <- nls(vbKt,data=dNU_FW07,start=svKt)
 
 vbL <- FL~Linf[sex]*(1-exp(-K*(age-t0)))
 svL <- Map(rep,svOm,c(2,1,1))
-fitL <- nls(vbL,data=dNU_FW,start=svL)
+fitL <- nls(vbL,data=dNU_FW07,start=svL)
 vbK <- FL~Linf*(1-exp(-K[sex]*(age-t0)))
 svK <- Map(rep,svOm,c(1,2,1))
-fitK <- nls(vbK,data=dNU_FW,start=svK)
+fitK <- nls(vbK,data=dNU_FW07,start=svK)
 vbt <- FL~Linf*(1-exp(-K*(age-t0[sex])))
 svt <- Map(rep,svOm,c(1,1,2))
-fitt <- nls(vbt,data=dNU_FW,start=svt)
+fitt <- nls(vbt,data=dNU_FW07,start=svt)
 
 extraSS(fitOm,com=fitLKt,sim.name="{Omega}",com.name="{Linf,K,t0}")
 extraSS(fitLK,fitLt,fitKt,com=fitLKt,com.name="{Linf,K,t0}",
@@ -67,11 +67,11 @@ cbind(Ests=coef(fitLK),confint(fitLK))
 vb <- vbFuns("typical")
 
 # females
-fem <- filterD(dNU_FW,sex=="F")
+fem <- filterD(dNU_FW07,sex=="F")
 svf <- list(Linf=733,K=0.23,t0=5.6)
 fitf <- nls(FL~vb(age,Linf,K,t0),data=fem,start=svf)
 # males
-mal <- filterD(dNU_FW,sex=="M")
+mal <- filterD(dNU_FW07,sex=="M")
 svm <- list(Linf=920,K=0.15,t0=5.6)
 fitm <- nls(FL~vb(age,Linf,K,t0),data=mal,start=svm)
 
@@ -85,7 +85,42 @@ curve(vb(x-offset,coef(fitf)),from=5,to=30,col=clr1[1],lwd=2,add=TRUE)
 # males
 points(FL~I(age+offset),data=mal,pch=19,col=clr2[2])
 curve(vb(x+offset,coef(fitm)),from=5,to=26,col=clr1[2],lwd=2,add=TRUE)
-legend("bottomright",levels(dNU_FW$sex),pch=19,col=clr1,bty="n",cex=0.8)
+legend("bottomright",levels(dNU_FW07$sex),pch=19,col=clr1,bty="n",cex=0.8)
+
+dNU_FW0710M <- filterD(dNU,!is.na(FL),!is.na(age),water.type %in% c("freshwater","Freshwater"),
+                       sex=="M",year %in% c(2007,2010))
+# Removed some obvious outliers
+dNU_FW0710M <- filterD(dNU_FW0710M,!(age>10 & FL<200))
+
+vbOm <- FL~Linf*(1-exp(-K*(age-t0)))
+svOm <- vbStarts(FL~age,data=dNU_FW0710M)
+fitOm <- nls(vbOm,data=dNU_FW0710M,start=svOm)
+
+svLKt <- Map(rep,svOm,c(2,2,2))
+vbLKt <- FL~Linf[fyear]*(1-exp(-K[fyear]*(age-t0[fyear])))
+fitLKt <- nls(vbLKt,data=dNU_FW0710M,start=svLKt)
+
+vbLK <- FL~Linf[fyear]*(1-exp(-K[fyear]*(age-t0)))
+svLK <- Map(rep,svOm,c(2,2,1))
+fitLK <- nls(vbLK,data=dNU_FW0710M,start=svLK)
+vbLt <- FL~Linf[fyear]*(1-exp(-K*(age-t0[fyear])))
+svLt <- Map(rep,svOm,c(2,1,2))
+fitLt <- nls(vbLt,data=dNU_FW0710M,start=svLt)
+vbKt <- FL~Linf*(1-exp(-K[fyear]*(age-t0[fyear])))
+svKt <- Map(rep,svOm,c(1,2,2))
+fitKt <- nls(vbKt,data=dNU_FW0710M,start=svKt)
+
+vbL <- FL~Linf[fyear]*(1-exp(-K*(age-t0)))
+svL <- Map(rep,svOm,c(2,1,1))
+fitL <- nls(vbL,data=dNU_FW0710M,start=svL)
+vbK <- FL~Linf*(1-exp(-K[fyear]*(age-t0)))
+svK <- Map(rep,svOm,c(1,2,1))
+fitK <- nls(vbK,data=dNU_FW0710M,start=svK)
+vbt <- FL~Linf*(1-exp(-K*(age-t0[fyear])))
+svt <- Map(rep,svOm,c(1,1,2))
+fitt <- nls(vbt,data=dNU_FW0710M,start=svt)
+
+extraSS(fitOm,com=fitLKt,sim.name="{Omega}",com.name="{Linf,K,t0}")
 
 
-# Script created at 2016-07-18 21:54:17
+# Script created at 2016-07-19 12:34:39
--- Exercises/Walleye/VBGF2.R
+++ Exercises/Walleye/VBGF2.R
@@ -84,5 +84,51 @@ points(tl~I(age+offset),data=mal,pch=19,col=clr2[2])
 curve(vb(x+offset,coef(fitm)),from=-2,to=10,col=clr1[2],lwd=2,add=TRUE)
 legend("bottomright",levels(dWE_13$sex),pch=19,col=clr1,bty="n",cex=0.8)
 
+dWE_1013 <- filterD(dWE,loc==1,year %in% c(2010,2013))
+dWE_1013 <- mutate(dWE_1013,fyear=factor(year))
 
-# Script created at 2016-07-18 21:20:54
+vbOm <- tl~Linf*(1-exp(-K*(age-t0)))
+svOm <- vbStarts(tl~age,data=dWE_1013)
+fitOm <- nls(vbOm,data=dWE_1013,start=svOm)
+
+svLKt <- Map(rep,svOm,c(2,2,2))
+vbLKt <- tl~Linf[fyear]*(1-exp(-K[fyear]*(age-t0[fyear])))
+fitLKt <- nls(vbLKt,data=dWE_1013,start=svLKt)
+
+vbLK <- tl~Linf[fyear]*(1-exp(-K[fyear]*(age-t0)))
+svLK <- Map(rep,svOm,c(2,2,1))
+fitLK <- nls(vbLK,data=dWE_1013,start=svLK)
+vbLt <- tl~Linf[fyear]*(1-exp(-K*(age-t0[fyear])))
+svLt <- Map(rep,svOm,c(2,1,2))
+fitLt <- nls(vbLt,data=dWE_1013,start=svLt)
+vbKt <- tl~Linf*(1-exp(-K[fyear]*(age-t0[fyear])))
+svKt <- Map(rep,svOm,c(1,2,2))
+fitKt <- nls(vbKt,data=dWE_1013,start=svKt)
+
+vbL <- tl~Linf[fyear]*(1-exp(-K*(age-t0)))
+svL <- Map(rep,svOm,c(2,1,1))
+fitL <- nls(vbL,data=dWE_1013,start=svL)
+vbK <- tl~Linf*(1-exp(-K[fyear]*(age-t0)))
+svK <- Map(rep,svOm,c(1,2,1))
+fitK <- nls(vbK,data=dWE_1013,start=svK)
+vbt <- tl~Linf*(1-exp(-K*(age-t0[fyear])))
+svt <- Map(rep,svOm,c(1,1,2))
+fitt <- nls(vbt,data=dWE_1013,start=svt)
+
+extraSS(fitOm,com=fitLKt,sim.name="{Omega}",com.name="{Linf,K,t0}")
+extraSS(fitLK,fitLt,fitKt,com=fitLKt,com.name="{Linf,K,t0}",
+        sim.names=c("{Linf,K}","{Linf,t0}","{K,t0}"))
+extraSS(fitK,fitt,com=fitLt,com.name="{K,t0}",
+        sim.names=c("{K}","{t0}"))
+
+# 2010
+plot(tl~I(age-offset),data=filterD(dWE_1013,year==2010),pch=19,col=clr2[1],
+     ylim=c(0,700),xlim=c(-2,16),xlab=xlbl,ylab=ylbl)
+curve(vb(x-offset,coef(fitLKt)[c(1,3,5)]),from=-2,to=16,col=clr1[1],lwd=2,add=TRUE)
+# males
+points(tl~I(age+offset),data=filterD(dWE_1013,year==2013),pch=19,col=clr2[2])
+curve(vb(x+offset,coef(fitLKt)[c(2,4,6)]),from=-2,to=16,col=clr1[2],lwd=2,add=TRUE)
+legend("bottomright",levels(dWE_1013$fyear),pch=19,col=clr1,bty="n",cex=0.8)
+
+
+# Script created at 2016-07-19 12:34:25

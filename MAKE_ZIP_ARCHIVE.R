287be7d680952f4ec74f24167e4c900536b7cd86
Derek Ogle <dogle@northland.edu>
2016-07-23 19:07:34 -0500

    Updated Zip Archives

--- MAKE_ZIP_ARCHIVE.R
+++ MAKE_ZIP_ARCHIVE.R
@@ -1,3 +1,11 @@
-setwd("C:/aaaWork/Web/GitHub/RCourseKC2016")
-file.remove("RCourseKC2016.zip")
-zip(zipfile="RCourseKC2016",files=c("Exercises/*.pdf","Handouts/*.pdf","Scripts/","Slides/"))
+setwd("C:/aaaWork/Web/GitHub/RCourseNunavut2016")
+## Just R scripts
+file.remove("RScriptsNunavut2016.zip")
+zip(zipfile="RScriptsNunavut2016",files=c("Scripts/"))
+
+## Entire course materials
+file.remove("RCourseNunavut2016.zip")
+zip(zipfile="RCourseNunavut2016",files=c("Slides/","Handouts/*.pdf","Scripts/",
+                                         "Exercises/*.pdf",
+                                         "Exercises/Nunavut/*.pdf","Exercises/Nunavut/*.xlsx","Exercises/Nunavut/*.csv",
+                                         "Exercises/Walleye/*.pdf","Exercises/Walleye/*.csv"))
